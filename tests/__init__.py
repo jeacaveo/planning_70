8dd5178a4b7359d84080f8cb6533ec505b7d694d
Jean Ventura <jv@venturasystems.net>
2014-04-27 08:34:44 -0400

    added tests module.

---
+++ tests/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for salon_spa module.
+
+This module groups a few sub-modules containing unittest2 test cases.
+
+Tests can be explicitely added to the `fast_suite` or `checks` lists or not.
+See the :ref:`test-framework` section in the :ref:`features` list.
+"""
+
+# import test_ir_sequence
+# import test_translate
+
+fast_suite = [
+    # test_ir_sequence,
+]
+
+checks = [
+    # test_translate,
+]
