6ff2351fa5d9ea6a37268a81e86b1bae556940e2
Jean Ventura <jv@venturasystems.net>
2014-04-28 15:22:20 -0400

    minor change to docs.

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,7 +8,7 @@ Tests can be explicitely added to the `fast_suite` or `checks` lists or not.
 See the :ref:`test-framework` section in the :ref:`features` list.
 
 To run tests:
-$ ./openerp-server -c .openerp_serverrc_marcos -d smalto_test -u salon_spa --log-level=test -i salon_spa --test-enable --test-report-directory=/tmp
+$ ./openerp-server -c .openerp_serverrc_marcos -d db -u module --log-level=test --test-enable
 
 """
 
